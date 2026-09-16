567e10c3c317ad0de5563f679ef2261f7de7dcf8
Asif-Abrar <asifabrar2@gmail.com>
2026-09-16 20:52:10 +0600

    Fix hydration warning

--- src/app/layout.js
+++ src/app/layout.js
@@ -23,7 +23,7 @@ export default function RootLayout({ children }) {
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body>
+      <body suppressHydrationWarning>
         <SiteChrome>
           {children}
         </SiteChrome>
